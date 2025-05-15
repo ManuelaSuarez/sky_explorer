78f4376132f79769be27f0e75aeb4ab38d68416e
MilagroLardone <milagrolardone@gmail.com>
2025-05-15 20:14:42 -0300

    Implementacion de logica de modal login y register

--- src/components/ModalLogin/ModalLogin.jsx
+++ src/components/ModalLogin/ModalLogin.jsx
@@ -1,10 +1,11 @@
 import "./ModalLogin.css";
 
-const ModalLogin = ({ onFlip, onClose }) => {
+const ModalLogin = ({ closeModal, openRegister }) => {
   return (
-    <div className="modal-card login-card">
+    <div className="background" onClick={closeModal}>
+      <div className="modal-card login-card" onClick={(e) => e.stopPropagation()}>
       <div className="modal-side login-left">
-        <span className="close" onClick={onClose}>&times;</span>
+        <span className="close" onClick={closeModal}>&times;</span>
         <h2>Iniciar Sesión</h2>
         <p className="description">Accede a tus viajes y experiencias personalizadas</p>
         <div className="input-group">
@@ -18,17 +19,19 @@ const ModalLogin = ({ onFlip, onClose }) => {
         <button className="login-button">Iniciar Sesión</button>
       </div>
       <div className="modal-side login-right">
-        <div className="overlay-shape">
-          <h2>¡Bienvenido a bordo!</h2>
-          <p>Regístrate para reservar vuelos, guardar destinos y más</p>
-          <button className="swap-button" onClick={onFlip}>
-            Registrarse
-          </button>
+          <div className="overlay-shape">
+            <h2>¡Bienvenido a bordo!</h2>
+            <p>Regístrate para reservar vuelos, guardar destinos y más</p>
+            <button className="swap-button" onClick={() => openRegister("register")}>
+              Registrarse
+            </button>
 
 
+          </div>
         </div>
       </div>
     </div>
+    
   );
 };
 
